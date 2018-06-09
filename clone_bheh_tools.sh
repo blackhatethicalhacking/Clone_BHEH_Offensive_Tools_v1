e95a6987f7db0255f697661dcf9a4c2e32144ea3
Black Hat Ethical Hacking <saintdrug@gmail.com>
2018-06-09 23:24:58 +0300

    Add files via upload
    Main Installation Script

---
+++ clone_bheh_tools.sh
@@ -0,0 +1,140 @@
+
+#!/bin/bash
+echo ''
+echo ''
+echo '  ____  _            _      _   _       _'
+echo ' | __ )| | __ _  ___| | __ | | | | __ _| |_'
+echo ' |  _ \| |/ _` |/ __| |/ / | |_| |/ _` | __|'
+echo ' | |_) | | (_| | (__|   <  |  _  | (_| | |_'
+echo ' |____/|_|\__,_|\___|_|\_\ |_| |_|\__,_|\__|'
+echo ''
+echo ' Ethical Hacking // blackhatethicalhacking.com'
+echo ' ---------------------------------------------'
+echo ' Welcome to our git cloner, for some of our'
+echo ' Black Hat Tools & more! Version 1.0'
+echo ' Written by SaintDruG'
+echo ''
+echo ' 10101010101010101010101010010101010101010101'
+echo ''
+echo 'Clone Some BHEH Fav Tools used in our daily training, pentesting.'
+echo 'Ranging from offensive scripts, reconnaissance, reporting, DDoS, Forensics & more.'
+echo ''
+echo 'The future of this script, as we are working on it, is to automate the installation of'
+echo 'all the cloned gits of some of our favorite most used tools, update your machine, using'
+echo 'a separate script that we have, by integrating it to this one, and eventually add to it'
+echo 'still it becomes an all in one tool, generating a report of the tools this script has' 
+echo 'installed listing them eventually in categories.'
+echo ''
+echo 'This is for educational purpose and testing only! Do not attempt using those if you do' 
+echo 'not have written authorization from the client! Black Hat Ethical Hacking is not to be held' 
+echo 'responsible for any of your actions!'
+echo ''
+printf '\033]2;INSTALLER\a'
+echo -e "Press \e[1;33many key\e[0m to install the script..."
+read -n 1
+mkdir bheh
+cd bheh
+git clone https://github.com/gentilkiwi/mimikatz.git
+git clone https://github.com/securityisamyth/pupy.git
+git clone https://github.com/securityisamyth/awesome-honeypots.git
+git clone https://github.com/securityisamyth/Azurite.git
+git clone https://github.com/securityisamyth/SleuthQL.git
+git clone https://github.com/securityisamyth/WAScan.git
+git clone https://github.com/securityisamyth/bheh_php_shell.git
+git clone https://github.com/securityisamyth/hate_crack.git
+git clone https://github.com/securityisamyth/Zoom.git
+git clone https://github.com/securityisamyth/arachni.git
+git clone https://github.com/securityisamyth/cloudflare-bypass.git
+git clone https://github.com/securityisamyth/filewatcher.git
+git clone https://github.com/securityisamyth/Hash-Buster.git
+git clone https://github.com/securityisamyth/howtoscanatargetblackhatstyle.git
+git clone https://github.com/securityisamyth/Dracnmap.git
+git clone https://github.com/securityisamyth/tweets_analyzer.git
+git clone https://github.com/securityisamyth/xerosploit.git
+git clone https://github.com/securityisamyth/PenBox.git
+git clone https://github.com/securityisamyth/claimsman.git
+git clone https://github.com/securityisamyth/mydlp.git
+git clone https://github.com/NorthernSec/CVE-Scan.git
+git clone https://github.com/vulnersCom/nmap-vulners.git
+git clone https://github.com/SpiderLabs/Responder.git
+git clone https://github.com/entropy1337/infernal-twin.git
+git clone https://github.com/AhMyth/AhMyth-Android-RAT.git
+git clone https://github.com/sqlmapproject/sqlmap.git
+git clone https://github.com/epsylon/ufonet.git
+git clone https://github.com/knownsec/Pocsuite.git
+git clone https://github.com/SecurityFTW/cs-suite.git
+git clone https://github.com/HatBashBR/HatCloud.git
+git clone https://github.com/medbenali/CyberScan.git
+git clone https://github.com/blackhatethicalhacking/AutoSploit.git
+git clone https://github.com/curi0usJack/luckystrike.git
+git clone https://github.com/danielmiessler/SecLists.git
+git clone https://github.com/samratashok/nishang.git
+git clone https://github.com/arxsys/dff.git
+git clone https://github.com/blackhatethicalhacking/antispoof.git
+git clone https://github.com/blackhatethicalhacking/antispoof.git
+git clone https://github.com/maxwellkoh/2FAssassin.git
+git clone https://github.com/AlisamTechnology/ATSCAN.git
+git clone https://github.com/4shadoww/hakkuframework.git
+git clone https://github.com/threat9/routersploit.git
+git clone https://github.com/adamcaudill/yawast.git
+git clone https://github.com/SerpicoProject/Serpico.git
+git clone https://github.com/juliocesarfort/public-pentesting-reports.git
+git clone https://github.com/PowerShellMafia/PowerSploit.git
+git clone https://github.com/PowerScript/KatanaFramework.git
+git clone https://github.com/jseidl/GoldenEye.git
+git clone https://github.com/LionSec/xerosploit.git
+git clone https://github.com/JiaoXianjun/BTLE.git
+git clone https://github.com/Screetsec/TheFatRat.git
+git clone https://github.com/dafthack/MailSniper.git
+git clone https://github.com/milo2012/phishing-frenzy-template-cloner.git
+git clone https://github.com/m4n3dw0lf/pythem.git
+git clone https://github.com/chihebchebbi/Internet-Of-Things-Pentesting-Framework.git
+git clone https://github.com/cuckoosandbox/cuckoo.git
+git clone https://github.com/aol/moloch.git
+git clone https://github.com/presidentbeef/brakeman.git
+git clone https://github.com/etsy/MIDAS.git
+git clone https://github.com/bro/bro.git
+git clone https://github.com/jipegit/OSXAuditor.git
+git clone https://github.com/sleuthkit/sleuthkit.git
+git clone https://github.com/ossec/ossec-hids.git
+git clone https://github.com/1N3/Sn1per.git
+git clone https://github.com/Tuhinshubhra/RED_HAWK.git
+git clone https://github.com/graniet/operative-framework.git
+git clone https://github.com/equalitie/BotHound.git
+git clone https://github.com/securestate/king-phisher.git
+git clone https://github.com/Yukinoshita47/Yuki-Chan-The-Auto-Pentest.git
+git clone https://github.com/mushorg/conpot.git
+git clone https://github.com/blackhatethicalhacking/owtf.git
+git clone https://github.com/WhitewidowScanner/whitewidow.git
+git clone https://github.com/susmithHCK/torghost.git
+git clone https://github.com/Shadowz3n/WPExploit.git
+git clone https://github.com/Veil-Framework/Veil.git
+git clone https://github.com/farhan3/py-botnet.git
+git clone https://github.com/Markus-Go/bonesi.git
+git clone https://github.com/pentestgeek/phishing-frenzy-templates.git
+git clone https://github.com/justjavac/ChromeSnifferPlus.git
+git clone https://github.com/hatRiot/zarp.git
+git clone https://github.com/RobinDavid/LSB-Steganography.git
+git clone https://github.com/Bitmessage/PyBitmessage.git
+git clone https://github.com/andresriancho/w3af.git
+git clone https://github.com/laramies/theHarvester.git
+git clone https://github.com/anonymous247742/TwitterReport.git
+git clone https://github.com/cobrateam/splinter.git
+git clone https://github.com/robertdavidgraham/masscan.git
+git clone https://github.com/EmpireProject/Empire.git
+git clone https://github.com/georgiaw/Smartphone-Pentest-Framework.git
+git clone https://github.com/websploit/websploit.git
+git clone https://github.com/offensive-security/exploit-database.git
+git clone https://github.com/gentilkiwi/mimikatz.git
+
+
+echo ""
+echo "Installation Has Been Completed... Enjoy ! :)"
+echo ""
+
+# Exit from the script with success (0)
+exit 0
+
+#written by SaintDruG for blackhatethicalhacking.com | All Rights Reserved.
+
+__ARCHIVE__
